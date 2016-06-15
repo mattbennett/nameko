6011e3efa5b335d2e467b5a1731862cca4a5cadc
Matt Bennett <matt@bennett.name>
2016-06-15 19:05:00 +0100

    add backoff example

--- docs/examples/test/test_examples.py
+++ docs/examples/test/test_examples.py
@@ -1,12 +1,38 @@
 """ Tests for the files and snippets in nameko/docs/examples
 """
 import os
+import re
 
+import arrow
+from kombu import Connection
+from kombu.pools import connections, producers
 from mock import call, patch
 
+from nameko.constants import AMQP_URI_CONFIG_KEY
 from nameko.standalone.events import event_dispatcher
 from nameko.standalone.rpc import ClusterRpcProxy, ServiceRpcProxy
 from nameko.testing.services import entrypoint_waiter
+
+
+def publisher(nameko_config, **kwargs):
+    """ Return a function that sends AMQP messages.
+    """
+    def publish(payload, routing_key, exchange=None):
+        """ Dispatch a message with `payload`
+        """
+        conn = Connection(nameko_config[AMQP_URI_CONFIG_KEY])
+
+        with connections[conn].acquire(block=True) as connection:
+            if exchange is not None:
+                exchange.maybe_bind(connection)
+            with producers[conn].acquire(block=True) as producer:
+                producer.publish(
+                    payload,
+                    routing_key=routing_key,
+                    exchange=exchange,
+                    **kwargs
+                )
+    return publish
 
 
 class TestHttp(object):
@@ -253,6 +279,60 @@ class TestTimer(object):
 
         with entrypoint_waiter(container, 'ping'):
             container.start()
+
+
+class TestBackoff(object):
+
+    def test_rpc(self, container_factory, rabbit_config):
+
+        from examples.backoff import Service
+
+        container = container_factory(Service, rabbit_config)
+        container.start()
+
+        timestamp = arrow.utcnow().replace(seconds=+1)
+
+        with ServiceRpcProxy('service', rabbit_config) as service_rpc:
+            res = service_rpc.method(timestamp.isoformat())
+        assert arrow.get(re.match("Time is (.+)", res).group(1)) >= timestamp
+
+    def test_event(self, container_factory, rabbit_config):
+
+        from examples.backoff import Service
+
+        container = container_factory(Service, rabbit_config)
+        container.start()
+
+        timestamp = arrow.utcnow().replace(seconds=+1)
+
+        dispatch = event_dispatcher(rabbit_config)
+        with entrypoint_waiter(
+            container, 'handle_event', callback=lambda _, res, exc: exc is None
+        ) as result:
+            payload = {'timestamp': timestamp.isoformat()}
+            dispatch("src_service", "event_type", payload)
+
+        res = result.get()
+        assert arrow.get(re.match("Time is (.+)", res).group(1)) >= timestamp
+
+    def test_message(self, container_factory, rabbit_config):
+
+        from examples.backoff import Service
+
+        container = container_factory(Service, rabbit_config)
+        container.start()
+
+        timestamp = arrow.utcnow().replace(seconds=+1)
+
+        publish = publisher(rabbit_config)
+        with entrypoint_waiter(
+            container, 'handle_message', callback=lambda _, res, exc: exc is None
+        ) as result:
+            payload = {'timestamp': timestamp.isoformat()}
+            publish(payload, routing_key="messages")
+
+        res = result.get()
+        assert arrow.get(re.match("Time is (.+)", res).group(1)) >= timestamp
 
 
 class TestTravis(object):
